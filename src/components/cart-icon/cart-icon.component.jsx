14ba385d49aea70f13808da930e33fa807d6bbd9
v833 <2507301541@qq.com>
2023-02-20 22:04:08 +0800

    feat: update navigations

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -13,7 +13,7 @@ const CartIcon = () => {
 
   return (
     <CartIconContainer onClick={toggleIsCartOpen}>
-      <ShoppingIcon className='shopping-icon' />
+      <ShoppingIcon />
       <ItemCount>{cartCount}</ItemCount>
     </CartIconContainer>
   )
