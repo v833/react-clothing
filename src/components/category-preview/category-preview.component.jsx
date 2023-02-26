6d09b9c824cdf267de8beaf93fb6af04d1081187
v833 <2507301541@qq.com>
2023-02-26 17:55:24 +0800

    feat: spinner component

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -3,21 +3,23 @@ import ProductCard from '../product-card/product-card.component'
 
 import './category-preview.styles.scss'
 
-const CategoryPreview = ({ title, products }) => (
-  <div className='category-preview-container'>
-    <h2>
-      <Link className='title' to={title}>
-        {title.toUpperCase()}
-      </Link>
-    </h2>
-    <div className='preview'>
-      {products
-        .filter((_, idx) => idx < 4)
-        .map((product) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
+const CategoryPreview = ({ title, products }) => {
+  return (
+    <div className='category-preview-container'>
+      <h2>
+        <Link className='title' to={title}>
+          {title.toUpperCase()}
+        </Link>
+      </h2>
+      <div className='preview'>
+        {products
+          .filter((_, idx) => idx < 4)
+          .map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default CategoryPreview
--- src/store/store.js
+++ src/store/store.js
@@ -9,7 +9,7 @@ import storage from 'redux-persist/lib/storage'
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['user']
+  blacklist: ['cart']
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer)
